39646c9829312a440b51cbc286d31dadb3fc3960
Suthep Sangvirotjanaphat <suthep@gfbd.co.th>
2015-07-08 22:58:49 +0700

    initial commit
    This project already has some code (Domain Models)
    and tests but not finished yet, please consider to add
    some more tests to drive your code further.

---
+++ CarFuel.Facts/FillUpFacts.cs
@@ -0,0 +1,84 @@
+﻿using CarFuel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CarFuel.Facts {
+  public class FillUpFacts {
+
+    public class GeneralUsage {
+
+      [Fact]
+      public void NewFillUp_HasCorrectInitialValues() {
+        // Arrange, Act
+        var f = new FillUp();
+
+        double liters = f.Liters;
+
+        Assert.Equal(0, f.Odometer);
+        Assert.Equal(0.0, liters);
+        Assert.True(f.IsFull);
+      }
+
+    }
+
+    public class KilometersPerLiterProperty {
+
+      //[Fact(Skip="Reason to skip this method")]
+      [Fact]
+      public void FirstFillUp_DontKnowKmL() {
+        // Arrange
+        var f = new FillUp();
+        f.Odometer = 1000;
+        f.Liters = 50;
+        f.IsFull = true;
+
+        // Act
+        double? kml = f.KilometersPerLiter;
+
+        // Assert
+        Assert.Null(kml);
+      }
+
+      [Fact]
+      public void SecondFillUps_TheFirstHasCorrectKmL() {
+        var f1 = new FillUp();
+        f1.Odometer = 1000;
+        f1.Liters = 50.0;
+        f1.IsFull = true;
+
+        var f2 = new FillUp();
+        f2.Odometer = 1500;
+        f2.Liters = 40;
+        f2.IsFull = true;
+
+        f1.NextFillUp = f2;
+
+        var kml1 = f1.KilometersPerLiter;
+        var kml2 = f2.KilometersPerLiter;
+
+        Assert.NotNull(kml1);
+        Assert.Equal(12.5, kml1);
+        Assert.Null(kml2);
+      }
+
+      [Fact]
+      public void ThirdFillUps_TheSecondHasCorrectKmL() {
+        var f2 = new FillUp(odometer: 1500, liters:40.0); 
+        var f3 = new FillUp(2250, 56, true); 
+        f2.NextFillUp = f3;
+
+        var kml1 = f2.KilometersPerLiter;
+        var kml2 = f3.KilometersPerLiter;
+
+        Assert.NotNull(kml1);
+        Assert.Equal(13.4, kml1);
+        Assert.Null(kml2);
+      }
+    }
+
+  }
+}
---
+++ CarFuel.Models/FillUp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarFuel.Models {
+  public class FillUp {
+    // 
+    public int Odometer { get; set; }
+
+    public double Liters { get; set; }
+
+    public bool? IsFull { get; set; }
+
+    public FillUp() : this(0, 0, true) {
+      // IsFull = true;
+    }
+
+    public FillUp(int odometer, double liters, bool isFull = true) { 
+      this.Odometer = odometer;
+      this.Liters = liters;
+      this.IsFull = isFull;
+    }
+
+    public double? KilometersPerLiter {
+      get {
+        if (this.NextFillUp == null) {
+          return null;
+        }
+        var kml = (NextFillUp.Odometer - this.Odometer)
+                  / NextFillUp.Liters;
+
+        return Math.Round(kml, 1, MidpointRounding.AwayFromZero);
+      }
+    }
+
+    public FillUp NextFillUp { get; set; }
+  }
+}
