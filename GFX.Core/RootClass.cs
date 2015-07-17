b7a09fe15f07ed86e996e8712a65a448d0db85c7
win <win05178@hotmail.com>
2015-07-17 10:27:49 +0700

    Improve GFX.Core
    by adding RootClass,IService,IService<T>,ServiceBase<T>,FakeRepository, and SystemTime

---
+++ GFX.Core/RootClass.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace GFX.Core
+{
+  public abstract class RootClass : IDisposable
+  {
+
+    public DbContext Context { get; set; }
+    private Dictionary<Type, object> _Services;
+
+
+    public RootClass(bool testing = false)
+    {
+      this.Context = this.NewDbContext();
+      this._Services = new Dictionary<Type, object>();
+
+      if (testing)
+      {
+        RegisterServicesForUnitTests();
+      }
+      else
+      {
+        RegisterServices();
+      }
+    }
+
+    public virtual void Dispose()
+    {
+      foreach (var item in _Services)
+      {
+        if (item.Value != null && item.Value is IDisposable)
+        {
+          ((IDisposable)(item.Value)).Dispose();
+        }
+      }
+      if (Context != null) Context.Dispose();
+    }
+
+    protected abstract DbContext NewDbContext();
+
+    protected abstract void RegisterServices();
+    protected abstract void RegisterServicesForUnitTests();
+
+    public int SaveChanges()
+    {
+      if (Context == null) return -1;
+      return Context.SaveChanges();
+    }
+
+    protected void AddService<TModel, TService, TRepository>()
+      where TModel : class
+      where TService : class, IService<TModel>, new()
+      where TRepository : class, IRepository<TModel>, new()
+    {
+
+      Type key = typeof(TService);
+
+      if (!_Services.ContainsKey(key))
+      {
+
+        Lazy<IService<TModel>> obj;
+        obj = new Lazy<IService<TModel>>(valueFactory: () =>
+        {
+          IService<TModel> x = new TService();  // T x = Activator.CreateInstance<T>(); 
+          x.Root = this;
+
+          if (x.RequiresOwnDbContext)
+          {
+            x.Context = this.NewDbContext();
+          }
+          else
+          {
+            x.Context = this.Context; // shared
+          }
+
+          x.Repository = new TRepository();
+          x.Repository.Context = this.Context;
+          return x;
+        });
+
+        _Services.Add(key: key, value: obj);
+      }
+    }
+
+    public TService Services<TModel, TService>()
+      where TModel : class
+      where TService : class, IService<TModel>
+    {
+
+      Type key = typeof(TService);
+
+      if (_Services.ContainsKey(key))
+      {
+        var lazy = (Lazy<IService<TModel>>)_Services[key];
+        return lazy.Value as TService;
+      }
+      else
+      {
+        var s = string.Format("Service '{0}' has not been registered to school.",
+          typeof(TService).Name);
+        throw new Exception(s);
+      }
+    }
+
+  }
+}
---
+++ GFX.Core/ServiceBase_T.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace GFX.Core
+{
+  public abstract class ServiceBase<T> : IService<T> where T : class
+  {
+
+    public ServiceBase()
+      : this(null)
+    {
+      //
+    }
+    public ServiceBase(RootClass root)
+    {
+      this.Root = root;
+    }
+
+    public RootClass Root { get; set; }
+    public DbContext Context { get; set; }
+
+    // factory method
+    public abstract IRepository<T> Repository { get; set; }
+
+    public virtual IQueryable<T> All()
+    {
+      return Query(_ => true);
+    }
+
+    public abstract T Find(params object[] keys);
+
+    public virtual IQueryable<T> Query(Func<T, bool> predicate)
+    {
+      if (Repository == null) return null;
+      return Repository.Query(predicate);
+    }
+
+
+    public virtual T Add(T item)
+    {
+      if (Repository == null) return null;
+      return Repository.Add(item);
+    }
+
+    public virtual void Remove(T item)
+    {
+      if (Repository == null) return;
+      Repository.Remove(item);
+    }
+
+    public virtual int SaveChanges()
+    {
+      if (Repository == null) return -1;
+      return Repository.SaveChanges();
+    }
+
+    public virtual bool RequiresOwnDbContext
+    {
+      get { return false; }
+    }
+
+    public virtual void SetModified(T item)
+    {
+      this.Context.Entry(item).State = System.Data.Entity.EntityState.Modified;
+    }
+
+    public virtual void SetAdded(T item)
+    {
+      this.Context.Entry(item).State = System.Data.Entity.EntityState.Added;
+    }
+
+  }
+}
